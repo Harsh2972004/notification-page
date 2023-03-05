51c20cfd468da9c78add3332770a65b69e814125
Harsh2972004 <108689578+Harsh2972004@users.noreply.github.com>
2023-03-05 18:40:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+$(".mark-btn").click(function(){
+    $(".unread").removeClass("unread");
+
+    $(".notification-count").fadeOut(300, function(){
+        $(this). remove();
+    });
+});
+
+var classes= $('.unread').length;
+console.log(classes);
+
+$(".unread-1").click(function(){
+    $(".unread-1").removeClass("unread");
+    classes = classes - 1
+    if (classes === 0) {
+        $(".notification-count").remove();
+    }
+    else {
+        $('.notification-count').text(classes);
+    }
+});
+
+$(".unread-2").click(function(){
+    $(".unread-2").removeClass("unread");
+    classes = classes - 1
+    if (classes === 0) {
+        $(".notification-count").remove();
+    }
+    else {
+        $('.notification-count').text(classes);
+    }
+});
+
+$(".unread-3").click(function(){
+    $(".unread-3").removeClass("unread");
+    classes = classes - 1
+    if (classes === 0) {
+        $(".notification-count").remove();
+    }
+    else {
+        $('.notification-count').text(classes);
+    }
+});
+
